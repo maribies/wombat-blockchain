2643c62babb7cfafc56ed3c72683727f2e09e7fc
Marissa Biescker <marissa.biesecker@gmail.com>
2021-03-27 17:24:29 +0000

    Implement /transactions/new
    Co-authored-by: Josh Cheek <josh.cheek@gmail.com>

--- lib/blockchain/app.rb
+++ lib/blockchain/app.rb
@@ -6,17 +6,36 @@ class Blockchain
   class App < Sinatra::Base
     BLOCKCHAIN = Blockchain.new()
 
+    set :default_content_type, 'application/json'
+
+    private def request_body
+      @request_body ||= request.body.read
+    end
+
+    private def request_data
+      @request_data ||= JSON.parse request_body, symbolize_names: true
+    end
+
     get '/mine' do
       "We'll mine a new Block"
     end
 
     post '/transactions/new' do
-      "We'll add a new transaction"
+      required = [:sender, :recipient, :amount]
+      halt 400, 'Missing values' unless required.all? { |k| request_data.key? k }
+
+      index = BLOCKCHAIN.new_transaction(
+        request_data[:sender],
+        request_data[:recipient],
+        request_data[:amount],
+      )
+
+      halt 201, JSON.dump({ message: "Transaction will be added to Block #{index}" })
     end
 
     get '/chain' do
       response = {
-        chain: BLOCKCHAIN.chain,
+        chain:  BLOCKCHAIN.chain,
         length: BLOCKCHAIN.chain.length,
       }
       JSON.dump(response)
